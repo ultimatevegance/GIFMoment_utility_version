c088b50c9ea8d902ab8ed09e38c6975c397f6f55
monster <xiongyongjie@howzhi.com>
2016-06-21 17:01:25 +0800

    initial commit :set up project

---
+++ GIFMoment/GIFMoment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIFMoment
+//
+//  Created by Monster on 6/21/16.
+//  Copyright © 2016 MonsterTech Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIFMoment/GIFMoment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIFMoment
+//
+//  Created by Monster on 6/21/16.
+//  Copyright © 2016 MonsterTech Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
